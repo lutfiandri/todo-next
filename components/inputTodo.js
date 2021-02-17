c95df443a39922850818eef280c452b2d8bbce56
Lutfi Andri <lutfiandri@pop-os.localdomain>
2021-02-17 20:32:29 +0700

    tailwind: clear input after close

--- components/inputTodo.js
+++ components/inputTodo.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 export default function InputTodo() {
   const [focus, setFocus] = useState(false);
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
 
   const handleClose = (e) => {
     e.preventDefault();
     setFocus(false);
+    setTitle('');
+    setDesc('');
   }
 
   return (
@@ -15,6 +19,8 @@ export default function InputTodo() {
         <input 
           type="text" 
           placeholder="Title" 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
           className={`${ !focus && 'hidden' } border-none focus:ring-0 font-semibold`} 
         />
         <textarea 
@@ -22,6 +28,8 @@ export default function InputTodo() {
           type="text" 
           rows={3}
           placeholder="Take a todo..." 
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
           className="border-none focus:ring-0 text-sm"
         />
         <div className={`${ !focus && 'hidden' } flex flex-row-reverse px-3 py-1 text-sm`}>
